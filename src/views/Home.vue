cf32a28af9efaac8bfc4bc7ed4c8d1ba831a4835
Marcus Hamill <marcusehamill@gmail.com>
2018-12-11 15:22:32 -0600

    add theme to index.html and app.vue, add theme elements to home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,366 @@
 <template>
   <div class="home">
-    <h1>{{ message }}</h1>
-    <!-- <div v-for="user in users"> -->
-      <a v-bind:href="'/#/users/show'" class="btn btn-secondary">profile</a>
-    <!-- </div> -->
+        <!-- SLIDE TOP -->
+    <div id="slidetop">
+
+      <div class="container">
+        
+        <div class="row">
+
+          <div class="col-md-4">
+            <h6><i class="icon-heart"></i> WHY TURNOUT?</h6>
+            <p>This app is simple to use and easy to share. Create your groups and events in no time. Designed to be simple and easy for you to use! </p>
+          </div>
+
+          <!-- <div class="col-md-4">
+            <h6><i class="fa-facheck"></i> RECENTLY VISITED</h6>
+            <ul class="list-unstyled">
+              <li><a href="#"><i class="fa fa-angle-right"></i> Consectetur adipiscing elit amet</a></li>
+              <li><a href="#"><i class="fa fa-angle-right"></i> This is a very long text, very very very very very very very very very very very very </a></li>
+              <li><a href="#"><i class="fa fa-angle-right"></i> Lorem ipsum dolor sit amet</a></li>
+              <li><a href="#"><i class="fa fa-angle-right"></i> Dolor sit amet,consectetur adipiscing elit amet</a></li>
+              <li><a href="#"><i class="fa fa-angle-right"></i> Consectetur adipiscing elit amet,consectetur adipiscing elit</a></li>
+            </ul>
+          </div> -->
+
+          <div class="col-md-4">
+            <h6><i class="icon-envelope"></i> CONTACT INFO</h6>
+            <ul class="list-unstyled">
+              <li><b>Address:</b> Chicagoland, IL</li>
+              <li><b>Phone:</b> 1-555-555-5555</li>
+              <li><b>Email:</b> <a href="mailto:marcusehamill@gmail.com">support</a></li>
+            </ul>
+          </div>
+
+        </div>
+
+      </div>
+
+      <a class="slidetop-toggle" href="#"><!-- toggle button --></a>
+
+    </div>
+    <!-- /SLIDE TOP -->
+    <div class="slider fullwidthbanner-container roundedcorners">
+          <!--
+            Navigation Styles:
+            
+              data-navigationStyle="" theme default navigation
+              
+              data-navigationStyle="preview1"
+              data-navigationStyle="preview2"
+              data-navigationStyle="preview3"
+              data-navigationStyle="preview4"
+              
+            Bottom Shadows
+              data-shadow="1"
+              data-shadow="2"
+              data-shadow="3"
+              
+            Slider Height (do not use on fullscreen mode)
+              data-height="300"
+              data-height="350"
+              data-height="400"
+              data-height="450"
+              data-height="500"
+              data-height="550"
+              data-height="600"
+              data-height="650"
+              data-height="700"
+              data-height="750"
+              data-height="800"
+          -->
+          <div class="fullwidthbanner" data-height="600" data-shadow="0" data-navigationStyle="preview1">
+            <ul class="hide">
+
+              <!-- SLIDE  -->
+              <li data-transition="fade" data-slotamount="1" data-masterspeed="1000" data-saveperformance="off" data-title="Slide title 1" data-thumb="demo_files/images/index-min.jpg">
+
+                <img src="assets/images/_smarty/1x1.png" data-lazyload="demo_files/images/index-min.jpg" alt="" data-bgfit="cover" data-bgposition="center bottom" data-bgrepeat="no-repeat" />
+
+                <div class="overlay dark-1"><!-- dark overlay [1 to 9 opacity] --></div>
+
+                <div class="tp-caption customin ltl tp-resizeme text_white"
+                  data-x="center"
+                  data-y="155"
+                  data-customin="x:0;y:150;z:0;rotationZ:0;scaleX:1;scaleY:1;skewX:0;skewY:0;opacity:0;transformPerspective:200;transformOrigin:50% 0%;"
+                  data-speed="800"
+                  data-start="1000"
+                  data-easing="easeOutQuad"
+                  data-splitin="none"
+                  data-splitout="none"
+                  data-elementdelay="0.01"
+                  data-endelementdelay="0.1"
+                  data-endspeed="1000"
+                  data-endeasing="Power4.easeIn" style="z-index: 10;">
+                  <span class="fw-300">Casual Meetups/Parties/Events/Night's Out</span>
+                </div>
+
+                <div class="tp-caption customin ltl tp-resizeme large_bold_white"
+                  data-x="center"
+                  data-y="205"
+                  data-customin="x:0;y:150;z:0;rotationZ:0;scaleX:1;scaleY:1;skewX:0;skewY:0;opacity:0;transformPerspective:200;transformOrigin:50% 0%;"
+                  data-speed="800"
+                  data-start="1200"
+                  data-easing="easeOutQuad"
+                  data-splitin="none"
+                  data-splitout="none"
+                  data-elementdelay="0.01"
+                  data-endelementdelay="0.1"
+                  data-endspeed="1000"
+                  data-endeasing="Power4.easeIn" style="z-index: 10;">
+                  WELCOME TO TURNOUT
+                </div>
+
+                <div class="tp-caption customin ltl tp-resizeme small_light_white font-lato"
+                  data-x="center"
+                  data-y="295"
+                  data-customin="x:0;y:150;z:0;rotationZ:0;scaleX:1;scaleY:1;skewX:0;skewY:0;opacity:0;transformPerspective:200;transformOrigin:50% 0%;"
+                  data-speed="800"
+                  data-start="1400"
+                  data-easing="easeOutQuad"
+                  data-splitin="none"
+                  data-splitout="none"
+                  data-elementdelay="0.01"
+                  data-endelementdelay="0.1"
+                  data-endspeed="1000"
+                  data-endeasing="Power4.easeIn" style="z-index: 10; width: 100%; max-width: 750px; white-space: normal; text-align:center; font-size:20px;">
+                  It's hard to plan even a simple gathering among friends. Everyone is busy. Turnout makes it easy to plan events in a few minutes. Take the guess work and endless group messages out of getting together.
+                </div>
+
+                <div class="tp-caption customin ltl tp-resizeme"
+                  data-x="center"
+                  data-y="383"
+                  data-customin="x:0;y:150;z:0;rotationZ:0;scaleX:1;scaleY:1;skewX:0;skewY:0;opacity:0;transformPerspective:200;transformOrigin:50% 0%;"
+                  data-speed="800"
+                  data-start="1550"
+                  data-easing="easeOutQuad"
+                  data-splitin="none"
+                  data-splitout="none"
+                  data-elementdelay="0.01"
+                  data-endelementdelay="0.1"
+                  data-endspeed="1000"
+                  data-endeasing="Power4.easeIn" style="z-index: 10;">
+                  <a href="/#/signup" class="btn btn-default btn-lg">
+                    <span>Join for free today!</span> 
+                  </a>
+                </div>
+
+              </li>
+
+              <!-- SLIDE  -->
+              <li data-transition="fade" data-slotamount="7" data-masterspeed="300"  data-saveperformance="off" data-title="Slide title 5" data-thumb="demo_files/images/wall2.jpg">
+                <img src="assets/images/_smarty/1x1.png" data-lazyload="demo_files/images/wall2.jpg" alt="" data-bgfit="cover" data-bgposition="left top" data-bgrepeat="no-repeat" />
+
+                <div class="tp-caption lfr" 
+                  data-x="left" data-hoffset="60"
+                  data-y="85" 
+                  data-speed="1500" 
+                  data-start="500" 
+                  data-easing="easeOutExpo" 
+                  data-elementdelay="0.1" 
+                  data-endelementdelay="0.1" 
+                  data-endspeed="300" 
+                  style="z-index: 2;">
+                  <img src="assets/images/_smarty/1x1.png" data-lazyload="demo_files/images/ipad.png" alt="" />
+                </div>
+
+                <div class="tp-caption block_black sft" 
+                  data-x="right" data-hoffset="-70"
+                  data-y="137" 
+                  data-speed="750" 
+                  data-start="1100" 
+                  data-easing="easeOutExpo" 
+                  data-splitin="none" 
+                  data-splitout="none" 
+                  data-elementdelay="0.1" 
+                  data-endelementdelay="0.1" 
+                  data-endspeed="300" 
+                  style="z-index: 3; max-width: auto; max-height: auto; white-space: nowrap;">Invite your friends
+                </div>
+
+                <div class="tp-caption block_black sfb" 
+                  data-x="right" data-hoffset="-70" 
+                  data-y="176" 
+                  data-speed="750" 
+                  data-start="1400" 
+                  data-easing="easeOutExpo" 
+                  data-splitin="none" 
+                  data-splitout="none" 
+                  data-elementdelay="0.1" 
+                  data-endelementdelay="0.1" 
+                  data-endspeed="300" 
+                  style="z-index: 4; max-width: auto; max-height: auto; white-space: nowrap;">Create a group
+                </div>
+
+                <div class="tp-caption block_theme_color sft" 
+                  data-x="right"  data-hoffset="-70"
+                  data-y="239" 
+                  data-speed="750" 
+                  data-start="1700" 
+                  data-easing="easeOutExpo" 
+                  data-splitin="none" 
+                  data-splitout="none" 
+                  data-elementdelay="0.1" 
+                  data-endelementdelay="0.1" 
+                  data-endspeed="300" 
+                  style="z-index: 5; max-width: auto; max-height: auto; white-space: nowrap;">Add your availability
+                </div>
+
+                <div class="tp-caption block_theme_color sfb" 
+                  data-x="right"  data-hoffset="-70"
+                  data-y="278" 
+                  data-speed="750" 
+                  data-start="2000" 
+                  data-easing="easeOutExpo" 
+                  data-splitin="none" 
+                  data-splitout="none" 
+                  data-elementdelay="0.1" 
+                  data-endelementdelay="0.1" 
+                  data-endspeed="300" 
+                  style="z-index: 6; max-width: auto; max-height: auto; white-space: nowrap;">Add your preferences for your events!
+                </div>
+
+                <div class="tp-caption block_black sft" 
+                  data-x="right"  data-hoffset="-70"
+                  data-y="340" 
+                  data-speed="750" 
+                  data-start="2300" 
+                  data-easing="easeOutExpo" 
+                  data-splitin="none" 
+                  data-splitout="none" 
+                  data-elementdelay="0.1" 
+                  data-endelementdelay="0.1" 
+                  data-endspeed="300" 
+                  style="z-index: 7; max-width: auto; max-height: auto; white-space: nowrap;">Let your friends know what you think!
+                </div>
+              </li>
+
+              <!-- SLIDE  -->
+              <li data-transition="fade" data-slotamount="7" data-masterspeed="300" data-link="http://www.stepofweb.com/" data-title="Slide title 4" data-target="_blank" data-saveperformance="off" data-thumb="demo_files/images/wall.jpg">
+
+                <img src="assets/images/_smarty/1x1.png" data-lazyload="demo_files/images/wall.jpg" alt="" data-bgfit="cover" data-bgposition="left top" data-bgrepeat="no-repeat" />
+
+                <div class="tp-caption lfb" 
+                  data-x="right" data-hoffset="-70" 
+                  data-y="120" 
+                  data-speed="2000" 
+                  data-start="500" 
+                  data-easing="easeOutExpo" 
+                  data-elementdelay="0.1" 
+                  data-endelementdelay="0.1" 
+                  data-endspeed="300" 
+                  style="z-index: 2;">
+                  <img src="assets/images/_smarty/1x1.png" alt="" data-lazyload="demo_files/images/desktop_slider.png">
+                </div>
+
+                <div class="tp-caption mediumlarge_light_white lfb tp-resizeme" 
+                  data-x="left" data-hoffset="150"
+                  data-y="183" 
+                  data-speed="1000" 
+                  data-start="1400" 
+                  data-easing="easeOutExpo" 
+                  data-splitin="none" 
+                  data-splitout="none" 
+                  data-elementdelay="0.1" 
+                  data-endelementdelay="0.1" 
+                  data-endspeed="300" 
+                  style="z-index: 3; max-width: auto; max-height: auto; white-space: nowrap;">No lengthy signup
+                </div>
+
+                <div class="tp-caption mediumlarge_light_white lft tp-resizeme" 
+                  data-x="left" data-hoffset="70"
+                  data-y="135" 
+                  data-speed="1000" 
+                  data-start="1200" 
+                  data-easing="easeOutExpo" 
+                  data-splitin="none" 
+                  data-splitout="none" 
+                  data-elementdelay="0.1" 
+                  data-endelementdelay="0.1" 
+                  data-endspeed="300" 
+                  style="z-index: 4; max-width: auto; max-height: auto; white-space: nowrap;">Always free to use!
+                </div>
+
+                <div class="tp-caption block_white sfl tp-resizeme" 
+                  data-x="70" 
+                  data-y="266" 
+                  data-speed="750" 
+                  data-start="1900" 
+                  data-easing="easeOutExpo" 
+                  data-splitin="none" 
+                  data-splitout="none" 
+                  data-elementdelay="0.1" 
+                  data-endelementdelay="0.1" 
+                  data-endspeed="300" 
+                  style="z-index: 5; max-width: auto; max-height: auto; white-space: nowrap;">100% Fully Responsive
+                </div>
+
+                <div class="tp-caption block_theme_color sfr tp-resizeme" 
+                  data-x="120" 
+                  data-y="310" 
+                  data-speed="750" 
+                  data-start="2200" 
+                  data-easing="easeOutExpo" 
+                  data-splitin="none" 
+                  data-splitout="none" 
+                  data-elementdelay="0.1" 
+                  data-endelementdelay="0.1" 
+                  data-endspeed="300" 
+                  style="z-index: 6; max-width: auto; max-height: auto; white-space: nowrap;">And Simply A Joy To Use
+                </div>
+
+                <div class="tp-caption block_white sfb tp-resizeme" 
+                  data-x="170" 
+                  data-y="356" 
+                  data-speed="750" 
+                  data-start="2500" 
+                  data-easing="easeOutExpo" 
+                  data-splitin="none" 
+                  data-splitout="none" 
+                  data-elementdelay="0.1" 
+                  data-endelementdelay="0.1" 
+                  data-endspeed="300" 
+                  style="z-index: 7; max-width: auto; max-height: auto; white-space: nowrap;">Why Not Try Turnout?
+                </div>
+              </li>
+            </ul>
+
+            <div class="tp-bannertimer"><!-- progress bar --></div>
+          </div>
+    </div>
+
+      <!-- -->
+    <section>
+
+      <div class="container">
+
+        <p>
+          Add to body: <code>.grain-green</code> class.
+        </p>
+        
+        <hr />
+
+
+        <p class="lead">Everyone you know is busy. It's hard to plan events or even a simple get together with friends. Turnout makes it simple.</p>
+        <p>We have all experienced it, the hassle of making plans with busy lives. Maybe you have that one friend who always forgets to mention things they have planned already. Or maybe you yourself are a bit indecisive. Let turnout simplify things, create a group, add friends by email, add your availability and then preferences. Boom. Turnout figures out the rest and lets you know when and what everyone in your group would like to do! It's that simple.</p>
+
+        <div class="row">
+          
+          <div class="col-md-6">
+            <p>Do you want to go out to eat? Do you want to see a movie? Let Turnout know how much and it will calculate based on your friends preferences what the group REALLY wants to do!</p>
+          </div>
+
+          <div class="col-md-6">
+            <p>View all events from your calendar page. Add new availability as your schedule changes. Invite new friends. No hassle signup and it's totally free!</p>
+          </div>
+
+        </div>
+
+      </div>
+    </section>
+    
+    <a href="#" id="toTop"></a>
+    
   </div>
 </template>
 
@@ -15,7 +372,7 @@ var axios = require('axios');
 export default {
   data: function() {
     return {
-      message: "Welcome to Vue.js!!!!",
+    
       users: [],
       user: {}
       
